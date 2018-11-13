0e25d871ceffaed9f982acce51aeb4623dadec81
thegreatfort <dgarrison247@gmail.com>
2018-11-13 16:14:59 -0600

    More work on Amazon home page
    -Fixed Accordion issue
    
    -Created splash gifs
    
    -fixed table Flow 3

---
+++ Amazon-RD.js
@@ -0,0 +1,41 @@
+// const dgcollapse = document.querySelector('.btn-link');
+
+// dgcollapse.addEventListener('click', function(e){
+
+// console.log('you clicked a card');
+// })
+
+
+// var acc = document.getElementsByClassName("accordionz");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//     acc[i].addEventListener("click", function() {
+//         /* Toggle between adding and removing the "active" class,
+//         to highlight the button that controls the panel */
+//         this.classList.toggle("active");
+
+//         /* Toggle between hiding and showing the active panel */
+//         var panel = this.nextElementSibling;
+//         if (panel.style.display === "block") {
+//             panel.style.display = "none";
+//         } else {
+//             panel.style.display = "block";
+//         }
+//     });
+// }
+
+var acc = document.getElementsByClassName("accordionz");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
